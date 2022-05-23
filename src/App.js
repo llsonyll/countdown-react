2b0733364592f85638d009a3462fa6945b68a050
llsonyll <sony_s07@hotmail.es>
2022-05-23 13:10:40 -0500

    JS countdown logic added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import github from "./assets/icon-pinterest.svg";
 
 // Footer Pattern
 import hillsPatter from "./assets/pattern-hills.svg";
+import bgStars from "./assets/bg-stars.svg";
+import { useEffect, useState } from "react";
 
 function App() {
   const footerStyle = {
@@ -17,15 +19,53 @@ function App() {
     backgroundSize: "cover",
   };
 
+  const contentStyle = {
+    backgroundImage: `url(${bgStars})`,
+    backgroundSize: "cover",
+    backgroundColor: "hsl(235, 16%, 14%)",
+    width: "100vw",
+    height: "100vh",
+  };
+
+  const formatTimeWithCero = (number) => {
+    if (number < 10) {
+      return `0${number}`;
+    }
+
+    return number;
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const countDownDate = new Date(new Date("Jun 24, 2022 15:37:25").getTime());
+
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
   return (
     <div className="App">
-      <main className="content">
+      <main className="content" style={contentStyle}>
         <div className="title">WE'RE LAUNCHING SOON</div>
         <div className="countdown">
-          <Counter number="05" label="Days" />
-          <Counter number="12" label="Hours" />
-          <Counter number="32" label="Minutes" />
-          <Counter number="11" label="Seconds" />
+          <Counter number={formatTimeWithCero(days)} label="Days" />
+          <Counter number={formatTimeWithCero(hours)} label="Hours" />
+          <Counter number={formatTimeWithCero(minutes)} label="Minutes" />
+          <Counter number={formatTimeWithCero(seconds)} label="Seconds" />
         </div>
       </main>
       <footer className="footer" style={footerStyle}>
